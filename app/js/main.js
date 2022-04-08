1d87b5fad76526f2037e173181fca4d902408b3e
Timur Aliev <timwolfskin013.gmail.com>
2022-04-08 09:07:36 +0300

    added blog section

--- app/js/main.js
+++ app/js/main.js
@@ -20,6 +20,11 @@ $(function () {
         arrows: false,
     });
 
+    $('.article-slider__box').slick({
+        prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/arrow-next.svg" alt=""></button>'
+    });
+
     var mixer = mixitup('.gallery__inner', {
         load: {
             filter: '.living'
